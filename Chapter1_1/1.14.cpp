0b56761864cf405962da0d25ff0a23a82cdbc6db
dovigod <dovimccalister@gmail.com>
2024-05-09 17:17:01 +0900

    1 end, preprocesor

---
+++ Chapter1_1/1.14.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+
+
+
+//preprocessor
+
+#define MY_N 9
+
+// macro, hardly, replace
+
+using namespace std;
+
+#define APPLE 3
+
+
+void main() {
+	cout << "Hello world 1.14" << endl;
+
+#ifdef APPLE
+	cout << "APPLE" << endl;
+#endif
+
+
+
+
+
+}
