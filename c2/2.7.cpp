2b60989c4dd5ef5eb9d0d9dc2969d0b12a5be68e
dovigod <dovimccalister@gmail.com>
2024-05-09 18:46:42 +0900

    2.7

---
+++ c2/2.7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+using namespace std;
+void main() {
+
+	cout << static_cast<char>(65) << endl;
+	cout << static_cast<int>('A') << endl;
+
+	// static cast <-> dynamic cast conversion between fundamental types
+
+
+
+}
