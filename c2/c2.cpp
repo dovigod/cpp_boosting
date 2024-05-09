befee7f0fd7c186a64c337961e48a3e30012c44f
dovigod <dovimccalister@gmail.com>
2024-05-09 17:28:40 +0900

    c2.1

---
+++ c2/c2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <bitset>
+
+int main()
+{
+    std::cout << "Hello World! chapter 2.1\n";
+
+    bool bVal = false;
+    char chValue = 65;
+    float fValue = 3.141592f;
+    // copy intialziation
+    double dValue = 3.141592;
+
+    auto x = dValue;
+
+    int a(123); // direct initialization
+    int b{ 123 }; // uniform initialization
+
+
+    int c(3.14); //warning; auto type coersion
+    int d{ 4.5 }; // error more strict
+
+
+}
