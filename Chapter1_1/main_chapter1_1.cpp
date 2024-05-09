a2b08f72f76f88e0e27aa99a3f6a0f569766374d
dovigod <dovimccalister@gmail.com>
2024-05-09 16:30:31 +0900

    chapter 1. standard i/o

--- Chapter1_1/main_chapter1_1.cpp
+++ Chapter1_1/main_chapter1_1.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
 int main() {
+	using namespace std;
+	int x = 3;
+	int pi = 3.14;
 
-	std::cout << "Chapter 1" << std::endl;
+	cout << "Chapter 1" << endl;
+	/*
+	cout << "I love this lecture!\n";
+	cout << "x is " << x << " p is " << pi << endl;
+	cout << "abc" << "\t" << " def" <<  endl;
+	cout << "ab" << "\t" << " cdef" << endl;
+
+	cout << "\a";*/
+
+	cin >> x;
+
+	cout << "Your input is " << x << endl;
+
 	return 0;
 }
